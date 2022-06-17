969074438b92ecdb7532f1d7a7eff0a7dfdfaf07
sathu341 <77712619+sathu341@users.noreply.github.com>
2022-06-17 18:01:53 +0530

    Add files via upload

---
+++ nav.jsx
@@ -0,0 +1,16 @@
+
+function Nav(){
+    return(
+ 
+       <>
+       <h1 className='btn_c'> Water Way</h1>
+       <a href="#">Register</a>
+       </>
+        
+     
+
+       
+    );
+}
+
+export default Nav
